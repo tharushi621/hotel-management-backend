ac13e12fc99ad57d3783941c744db60d6bdde056
Tharu <tharubhagya621@gmail.com>
2025-06-23 21:27:33 +0530

    Create categoryRoute.js

---
+++ routes/categoryRoute.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import { createCategory, deleteCategory, getCategory, getCategoryByName } from '../controllers/CategoryController.js'
+
+const categoryRouter = express.Router()
+
+categoryRouter.post("/",createCategory)
+categoryRouter.delete("/:name",deleteCategory)
+categoryRouter.get("/:name",getCategoryByName)
+categoryRouter.get("/",getCategory)
+
+export default categoryRouter;
+
