ab6035d4552e64e24e5e7432a1daf2a718c2b500
Tharu <tharubhagya621@gmail.com>
2025-06-23 23:09:25 +0530

    Update categoryController.js

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -103,3 +103,37 @@ export function getCategoryByName(req,res){
         }
     )
 }
+export function updateCategory(req,res){
+    
+    if(!isAdminValid){
+        res.status(403).json({
+            message:"Unauthorized"
+        })
+        return
+    }
+    const name = req.params.name;
+    Category.updateOne({name:name},req.body).then(
+        ()=>{
+res.json({
+    message:"Category updated successfully"
+})
+        }
+    ).catch(
+        ()=>{
+            res.json({
+                message:"Failed to update the category"
+            })
+        }
+    )
+}
+
+
+function isAdminValid(req){
+    if(req.user == null){
+        return false
+    }
+    if(req.user.type != "admin"){
+        return false
+    }
+    return true;
+}
