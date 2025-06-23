a6be403d91c42e835b794ecb2a0134376fb2088f
Tharu <tharubhagya621@gmail.com>
2025-06-24 03:42:58 +0530

    Delete galleryItemRoute.js

--- routes/galleryItemRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import {createGalleryItem, getGalleryItems,} from '../controllers/galleryItemController.js'
-
-const galleyItemRouter = express.Router()
-
-galleyItemRouter.post("/",createGalleryItem)
-galleyItemRouter.get("/",getGalleryItems)
-
-export default galleyItemRouter;
