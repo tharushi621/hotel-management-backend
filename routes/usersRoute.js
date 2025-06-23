fe73cd233a9da1c77b3bf800eac1517d7ad5077b
Tharu <tharubhagya621@gmail.com>
2025-06-24 03:42:53 +0530

    Delete usersRoute.js

--- routes/usersRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import {loginUser, postUsers} from '../controllers/userController.js'
-
-const userRouter = express.Router()
-
-userRouter.post("/",postUsers)
-userRouter.post("/login",loginUser)
-
-export default userRouter;
